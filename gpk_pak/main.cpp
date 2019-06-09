2534e588b163d082b7c8cd69a2eef6a97754c43a
asm128 <pabloarielz@outlook.com>
2019-06-09 16:13:10 -0300

    + Upgraded gpk_pak and gpk_unpak.

--- gpk_pak/main.cpp
+++ gpk_pak/main.cpp
@@ -8,84 +8,20 @@
 #include "gpk_coord.h"
 #include "gpk_deflate.h"
 
-int main(int argc, char ** argv) { 
-	if(2 > argc) {
-		printf("Usage:\n\t%s [input folder name] [output file name (optional)]", argv[0]);
-		return -1;
-	}
-	char							nameFileDst[512]		= {};	// compose filename
+int										main						(int argc, char ** argv)	{
+	ree_if(2 > argc, "Usage:\n\t%s [input folder name] [output file name (optional)]", argv[0]);
+
+	char										nameFileDst[512]			= {};	// compose filename
 	if(2 < argc)
 		sprintf_s(nameFileDst, "%s", argv[2]);
 	else
 		sprintf_s(nameFileDst, "%s.gpk", argv[1]);
 
-	::gpk::SPackHeader 				fileHeader;
-	// -- The following two arrays store the file table and the file contents that are going to be compressed and stored on disk
-	::gpk::array_pod<byte_t>		tableFiles				= {};
-	::gpk::array_pod<byte_t>		contentsPacked			= {};
-	{
-		::gpk::SPathContents			pathTree				= {};
-		const ::gpk::view_const_string	nameFolderSrc			= {argv[1], (uint32_t)strlen(argv[1])};;
-		gpk_necall(::gpk::pathList(nameFolderSrc, pathTree), "Failed to list folder: %s.", nameFolderSrc.begin());
-		::gpk::array_obj<::gpk::array_pod<char_t>>	listFiles				= {};
-		gpk_necall(::gpk::pathList(pathTree, listFiles), "Failed to list folder: %s.", nameFolderSrc.begin());
-		//gpk_necall(::gpk::pathList(nameFolderSrc, listFiles, false), "Failed to list folder: %s.", nameFolderSrc);
-		::gpk::array_pod<byte_t>		contentsTemp			= {};
-		::gpk::SRange<uint32_t>			fileLocation			= {0, 0};
-		for(uint32_t iFile = 0; iFile < listFiles.size(); ++iFile) {
-			fileLocation.Offset			= contentsPacked.size();
-			const ::gpk::view_const_string	pathToLoad			= {listFiles[iFile].begin(), listFiles[iFile].size()};
-			if(0 == pathToLoad.size())
-				continue;
-			printf("pathToLoad (%u): '%s'.\n", iFile, pathToLoad.begin());
-			gpk_necall(::gpk::fileToMemory(pathToLoad, contentsTemp), "Failed to load file: %s. Out of memory?", pathToLoad.begin());
+	const ::gpk::view_const_string				nameFolderSrc				= {argv[1], (uint32_t)strlen(argv[1])};;
+	::gpk::SFolderPackage						folderPackage;
+	gpk_necall(::gpk::folderPack(folderPackage, nameFolderSrc), "Failed to pack folder: %s.", nameFolderSrc.begin());
+	gpk_necall(::gpk::folderToDisk(folderPackage, nameFileDst), "Failed to pack folder: %s.", nameFolderSrc.begin());
 
-			fileLocation.Count			= contentsTemp.size();
-			gpk_necall(tableFiles.append((const byte_t*)&fileLocation, sizeof(::gpk::SRange<uint32_t>)), "Failed to append data! %s.", "Out of memory?");
-			uint32_t						pathLen					= pathToLoad.size();
-			gpk_necall(tableFiles.append((const byte_t*)&pathLen, sizeof(uint32_t)), "Failed to append data bytes. Buffer sizes:"
-				"tableFiles     : %u."
-				"contentsPacked : %u."
-				, tableFiles		.size()
-				, contentsPacked	.size()
-				);
-			gpk_necall(tableFiles.append(pathToLoad.begin(), pathLen), "Failed to append data bytes. Buffer sizes:"
-				"tableFiles     : %u."
-				"contentsPacked : %u."
-				, tableFiles		.size()
-				, contentsPacked	.size()
-				);
-			gpk_necall(contentsPacked.append(contentsTemp.begin(), contentsTemp.size()), "Failed to append data bytes. Buffer sizes:"
-				"tableFiles     : %u."
-				"contentsPacked : %u."
-				, tableFiles		.size()
-				, contentsPacked	.size()
-				);
-			contentsTemp.clear();
-			++fileHeader.TotalFileCount;
-		}
-	}
-	fileHeader.SizeUncompressedTableFiles		= tableFiles		.size();
-	fileHeader.SizeUncompressedContentsPacked	= contentsPacked	.size();
-	::gpk::array_pod<byte_t>		compressedTableFiles		= {};
-	::gpk::array_pod<byte_t>		compressedContentsPacked	= {};
-	compressedTableFiles		.resize(tableFiles		.size()	);
-	compressedContentsPacked	.resize(contentsPacked	.size()	);
-	{	// compress
-		gpk_necall(::gpk::arrayDeflate(tableFiles		, compressedTableFiles		), "%s", "Unknown error.");
-		gpk_necall(::gpk::arrayDeflate(contentsPacked	, compressedContentsPacked	), "%s", "Unknown error.");
-		fileHeader.SizeCompressedTableFiles		= compressedTableFiles		.size();
-		fileHeader.SizeCompressedContentsPacked	= compressedContentsPacked	.size();
-	}
-	{
-		FILE							* fp					= 0;
-		fopen_s(&fp, nameFileDst, "wb");
-		ree_if(0 == fp, "Failed to create file: %s.", nameFileDst);
-		fwrite(&fileHeader		, 1, sizeof(::gpk::SPackHeader)				, fp);
-		fwrite(compressedTableFiles		.begin	(), 1, compressedTableFiles		.size()	, fp);
-		fwrite(compressedContentsPacked	.begin	(), 1, compressedContentsPacked	.size()	, fp);
-		fclose(fp);
-	}
 	printf("Successfully written '%s'?", nameFileDst);
 	return 0; 
 }
--- gpk_unpak/main.cpp
+++ gpk_unpak/main.cpp
@@ -8,70 +8,22 @@
 #include "gpk_coord.h"
 #include "gpk_deflate.h"
 
-struct SGPKPak {
-	::gpk::array_pod<byte_t>							DataContents	;
-	::gpk::array_pod<byte_t>							DataInfo		;
-	::gpk::array_pod<::gpk::view_array<const char_t>>	Contents		;
-	::gpk::array_pod<::gpk::view_const_string>			Names			;
-};
-
-int											main				(int argc, char ** argv)						{
-	if(2 > argc) {
-		printf("Usage:\n\t%s [input file name] [output folder (optional)]", argv[0]);
-		return -1;
-	}
-	char										nameFileSrc	[4096]		= {};
-	const int32_t								sizeNameSrc				= (int32_t)sprintf_s(nameFileSrc, "%s", argv[1]);	
-
-	char										namePathDst	[4096]		= {};
+int										main							(int argc, char ** argv)						{
+	ree_if(2 > argc, "Usage:\n\t%s [input file name] [output folder (optional)]", argv[0]);
+	char										nameFileSrc	[4096]				= {};
+	const int32_t								sizeNameSrc						= (int32_t)sprintf_s(nameFileSrc, "%s", argv[1]);	
+	char										namePathDst	[4096]				= {};
 	sprintf_s(namePathDst, "%s", argv[2]);
 	if(2 == argc) {
-		sprintf_s(namePathDst, "%s", argv[2]);
-		::gpk::error_t								indexSequence			= ::gpk::rfind_sequence_pod(::gpk::view_array<const char_t>{".gpk"}, ::gpk::view_array<const char_t>{nameFileSrc});
-		if(-1 != indexSequence)
-			namePathDst[indexSequence]				= 0;
-	}
-	SGPKPak										virtualFolder			= {};
-	{
-		::gpk::array_pod<byte_t>					rawFileInMemory			= {};
-		gpk_necall(::gpk::fileToMemory(nameFileSrc, rawFileInMemory), "Failed to load pak file: %s.", nameFileSrc);
-		const ::gpk::SPackHeader					& header				= *(::gpk::SPackHeader*)&rawFileInMemory[0];
-		virtualFolder.DataInfo		.resize(header.SizeUncompressedTableFiles		);
-		virtualFolder.DataContents	.resize(header.SizeUncompressedContentsPacked	);
-		virtualFolder.Names			.resize(header.TotalFileCount);
-		virtualFolder.Contents		.resize(header.TotalFileCount);
-		gpk_necall(::gpk::arrayInflate({&rawFileInMemory[0] + sizeof(::gpk::SPackHeader)									, header.SizeCompressedTableFiles		}, virtualFolder.DataInfo		), "Failed to uncompress file table.");
-		gpk_necall(::gpk::arrayInflate({&rawFileInMemory[0] + sizeof(::gpk::SPackHeader) + header.SizeCompressedTableFiles	, header.SizeCompressedContentsPacked	}, virtualFolder.DataContents	), "Failed to uncompress file contents.");
-	}
-
-	{
-		uint32_t							offsetInfo		= 0;
-		for(uint32_t iFile = 0; iFile < virtualFolder.Names.size(); ++iFile) {
-			const ::gpk::SRange<uint32_t>		& fileLocation			= *(const ::gpk::SRange<uint32_t>*)&virtualFolder.DataInfo[offsetInfo];
-			offsetInfo						+= sizeof(const ::gpk::SRange<uint32_t>);
-			const uint32_t						lenName					= *(uint32_t*)&virtualFolder.DataInfo		[offsetInfo];
-			offsetInfo						+= sizeof(uint32_t);
-			virtualFolder.Names			[iFile] = {&virtualFolder.DataInfo		[offsetInfo], lenName};
-			offsetInfo						+= lenName;
-
-			virtualFolder.Contents		[iFile] = {&virtualFolder.DataContents	[fileLocation.Offset], fileLocation.Count};
+		sprintf_s(namePathDst, "%s", argv[1]);
+		::gpk::error_t								indexSequence					= ::gpk::rfind_sequence_pod(::gpk::view_const_string{".gpk"}, ::gpk::view_const_string{nameFileSrc});
+		if(-1 != indexSequence) {
+			namePathDst[indexSequence]					= '\\';
+			namePathDst[indexSequence + 1]				= 0;
 		}
 	}
-	for(uint32_t iFile = 0, countFiles = virtualFolder.Names.size(); iFile < countFiles; ++iFile) {
-		info_printf("File found (%u): %s. Size: %u.", iFile, virtualFolder.Names[iFile].begin(), virtualFolder.Contents[iFile].size());
-		char					pathName [4096];
-		sprintf_s(pathName, virtualFolder.Names[iFile].begin());
-		::gpk::error_t			indexSlash = ::gpk::rfind_sequence_pod(::gpk::view_array<const char>{"\\", 1}, ::gpk::view_array<const char>{pathName});
-		if(-1 != indexSlash) {
-			pathName[indexSlash + 1] = 0;
-			::gpk::dirCreate(pathName);
-		}
-
-		FILE	* fp = 0;
-		fopen_s(&fp, virtualFolder.Names[iFile].begin(), "wb");
-		ce_if(0 == fp, "Failed to create file: %s.", virtualFolder.Names[iFile].begin());
-		fwrite(virtualFolder.Contents[iFile].begin(), 1, virtualFolder.Contents[iFile].size(), fp);
-		fclose(fp);
-	}
+	::gpk::SFolderInMemory						virtualFolder					= {};
+	gpk_necall(::gpk::folderUnpack(virtualFolder, ::gpk::view_const_string{nameFileSrc}), "Failed to unpack file: %s.", nameFileSrc);
+	gpk_necall(::gpk::folderToDisk(virtualFolder, namePathDst), "Failed to write folder to disk. Disk full or insufficient permissions. File name: %s. Destionation Path: %s.", nameFileSrc, namePathDst);
 	return 0; 
 }
