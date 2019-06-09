873a34bc9d4e6215b461e4caf89ab9bb3a5280a7
asm128 <pabloarielz@outlook.com>
2019-06-08 23:51:30 -0300

    + Added gpk_splitter tool. + Improved gpk_joiner tool.

--- gpk_joiner/main.cpp
+++ gpk_joiner/main.cpp
@@ -1,26 +1,22 @@
+#define GPK_CONSOLE_LOG_ENABLED
+
 #include "gpk_log.h"
 #include "gpk_storage.h"
 
 int main(int argc, char ** argv) { 
-	if(2 > argc) {
-		printf("Usage:\n\t%s [filename]", argv[0]);
-		return -1;
-	}
-
+	ree_if(2 > argc, "Usage:\n\t%s [filename]", argv[0]);
 	const char						* fileNameDst		= argv[1];
 	char							fileNameSrc	[1024]	= {};
 	uint32_t						iFile				= 0;
-	sprintf_s(fileNameSrc, "%s.splitted.%.2u", fileNameDst, iFile);
+	sprintf_s(fileNameSrc, "%s.split.%.2u", fileNameDst, iFile);
 	::gpk::array_pod<byte_t>		fileInMemory;
-	
 	FILE							* fpDest;
 	fopen_s(&fpDest, fileNameDst, "wb");
-	reterr_error_if(0 == fpDest, "Failed to create file: %s.", fileNameDst);
+	ree_if(0 == fpDest, "Failed to create file: %s.", fileNameDst);
 	while(0 == ::gpk::fileToMemory(fileNameSrc, fileInMemory)) {
 		fwrite(fileInMemory.begin(), 1, fileInMemory.size(), fpDest);
-		sprintf_s(fileNameSrc, "%s.splitted.%.2u", fileNameDst, ++iFile);
+		sprintf_s(fileNameSrc, "%s.split.%.2u", fileNameDst, ++iFile);
 	}
-
 	fclose(fpDest);
 	return 0; 
 }
