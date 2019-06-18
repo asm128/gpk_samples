b19808e1da81e4348a87406f87aafdc2a64ad3ee
asm128 <pabloarielz@outlook.com>
2019-06-18 19:23:22 -0300

    + Minor formtting fixes.

--- test_gpk_json/main.cpp
+++ test_gpk_json/main.cpp
@@ -12,14 +12,15 @@
 	info_printf(bufferFormat, node->Object->Type, ::gpk::get_value_label(node->Object->Type).begin(), node->Object->Span.Begin, node->Object->Span.End, node->Object->ParentIndex, node->ObjectIndex, &testJson[node->Object->Span.Begin]);
 	for(uint32_t iChildren = 0; iChildren < node->Children.size(); ++iChildren)
 		::printNode(node->Children[iChildren], testJson);
+
 	return 0;
 }
 
 int											main						()			{
-	static const ::gpk::view_const_string			testJson					= //"[{\"\":[123,]}]";
+	static const ::gpk::view_const_string			testJson					= //"[{\"a\":[123 3,32,1156]}]";
 		//"\n[ { \"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
 		"\n[ { \"Bleh\": \"test\" }, {}, {   }, [  ]  , [], {\"a\":{}, \"b\":[123,]}"
-		"\n, { \"NameId\" : \"ASD\", \"Bleh\":[234124,123,243234, 321   , 65198], \"Else\": [{\"Object\": false}, {}],\"Something\" : \"out\",}"
+		"\n, { \"NameId\" : \"ASD\" \"\", \"Bleh\" :[234124,123,243234, 321   , 65198], \"Else\": [{\"Object\": false}, {}],\"Something\" : \"out\",}"
 		"\n, { \"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
 		"\n, { \"NameId\" : true, \"B\\\"leh\": null, \"Else\": false} "
 		"\n, { \"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
@@ -35,7 +36,7 @@ int											main						()			{
 
 	info_printf("---------------------------- Linear iteration: %u objects.", jsonReader.Object.size());
 	::gpk::array_pod<char_t>						outputJson;
-	::gpk::ptr_obj<::gpk::SJSONNode>					root							= jsonReader.Tree[0];
+	::gpk::ptr_obj<::gpk::SJSONNode>				root							= jsonReader.Tree[0];
 	::printNode(root, testJson);
 
 	info_printf("---------------------------- Linear iteration: %u objects.", jsonReader.Object.size());
@@ -52,16 +53,16 @@ int											main						()			{
 		info_printf(bufferFormat, node.Type, ::gpk::get_value_label(node.Type).begin(), node.Span.Begin, node.Span.End, node.ParentIndex, iNode, view.begin());
 	}
 
-	const ::gpk::view_const_string						test_key						= "Bleh";
+	const ::gpk::view_const_string					test_key						= "Bleh";
 	info_printf("---------------------------- Access test. Test key: %s.", test_key.begin());
-	::gpk::error_t										indexOfFirstObject				= ::gpk::jsonArrayValueGet(*root, 0);
-	::gpk::ptr_obj<::gpk::SJSONNode>					object							= jsonReader.Tree[indexOfFirstObject];
-	::gpk::error_t										indexOfElement					= ::gpk::jsonObjectValueGet(*object, jsonReader.View, test_key);
+	::gpk::error_t									indexOfFirstObject				= ::gpk::jsonArrayValueGet(*root, 0);
+	::gpk::ptr_obj<::gpk::SJSONNode>				object							= jsonReader.Tree[indexOfFirstObject];
+	::gpk::error_t									indexOfElement					= ::gpk::jsonObjectValueGet(*object, jsonReader.View, test_key);
 	gpk_necall((uint32_t)indexOfElement >= jsonReader.View.size(), "Test key '%s' not found: %i.", test_key.begin(), indexOfElement);
-	const gpk::SJSONType								& node							= jsonReader.Object	[indexOfElement];	
-	::gpk::view_const_string							view							= jsonReader.View	[indexOfElement - 1];	// get the parent in order to retrieve the view with the surrounding in the case of strings "".
-	char												bufferFormat [8192]				= {};
-	uint32_t											lenString						= view.size();
+	const gpk::SJSONType							& node							= jsonReader.Object	[indexOfElement];	
+	::gpk::view_const_string						view							= jsonReader.View	[indexOfElement - 1];	// get the parent in order to retrieve the view with the surrounding in the case of strings "".
+	char											bufferFormat [8192]				= {};
+	uint32_t										lenString						= view.size();
 	sprintf_s(bufferFormat, "Found test value {'%%s':%%%u.%us}. Node type: %%u (%%s). Node Span: {%%u, %%u}. Parent index: %%u. Object index: %%u.", lenString, lenString);
 	info_printf(bufferFormat, test_key.begin(), view.begin(), node.Type, ::gpk::get_value_label(node.Type).begin(), node.Span.Begin, node.Span.End, node.ParentIndex, indexOfElement, view.begin());
 	return 0;
