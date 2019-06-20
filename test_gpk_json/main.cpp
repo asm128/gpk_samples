5597d7f0082e6e6a9ee465cdbef04b594cb23425
asm128 <pabloarielz@outlook.com>
2019-06-20 17:06:07 -0300

    - Removed code which is already intregrated into the latest revision of gpk.

--- test_gpk_json/main.cpp
+++ test_gpk_json/main.cpp
@@ -4,10 +4,11 @@
 #define GPK_INFO_PRINTF_ENABLED
 
 #include "gpk_json.h"
+#include "gpk_json_formatter.h"
 
-::gpk::error_t								printNode					(::gpk::SJSONNode* node, const ::gpk::view_const_char& testJson)			{
-	char											bufferFormat [1024]			= {};
-	uint32_t										lenString					= node->Object->Span.End - node->Object->Span.Begin;
+::gpk::error_t								printNode						(::gpk::SJSONNode* node, const ::gpk::view_const_char& testJson)			{
+	char											bufferFormat [1024]				= {};
+	uint32_t										lenString						= node->Object->Span.End - node->Object->Span.Begin;
 	sprintf_s(bufferFormat, "Node type: %%u (%%s). Node Span: {%%u, %%u}. Parent index: %%u. Object index: %%u. Text: %%%u.%us", lenString, lenString);
 	info_printf(bufferFormat, node->Object->Type, ::gpk::get_value_label(node->Object->Type).begin(), node->Object->Span.Begin, node->Object->Span.End, node->Object->ParentIndex, node->ObjectIndex, &testJson[node->Object->Span.Begin]);
 	for(uint32_t iChildren = 0; iChildren < node->Children.size(); ++iChildren)
@@ -16,90 +17,6 @@
 	return 0;
 }
 
-namespace gpk
-{
-	GDEFINE_ENUM_TYPE(JSON_FORMATTER_TYPE, int8_t);
-	GDEFINE_ENUM_VALUE(JSON_FORMATTER_TYPE, LITERAL	, 0);
-	GDEFINE_ENUM_VALUE(JSON_FORMATTER_TYPE, TOKEN	, 1);
-	GDEFINE_ENUM_VALUE(JSON_FORMATTER_TYPE, COUNT	, 2);
-	GDEFINE_ENUM_VALUE(JSON_FORMATTER_TYPE, UNKNOWN	, -1);
-
-	struct SJSONFormatterType {
-		int32_t												ParentIndex;
-		JSON_FORMATTER_TYPE									Type;
-		::gpk::SSlice<uint32_t>								Span;
-	};
-
-	struct SJSONFormatterState {
-		uint32_t											IndexCurrentChar		= 0;
-		int32_t												IndexCurrentElement		= -1;
-		::gpk::SJSONFormatterType							* CurrentElement		= 0;
-		int32_t												NestLevel				= 0;
-		char												CharCurrent				= 0;
-		bool												Escaping				= false;
-		bool												InsideToken				= false;
-	};
-}
-
-::gpk::error_t								jsonStringFormatParseToken		(::gpk::SJSONFormatterState & work_state, ::gpk::array_pod<::gpk::SJSONFormatterType> & out_types, const ::gpk::view_const_string& in_format)		{ 
-	in_format;
-	switch(work_state.CharCurrent) {
-	default		: break;
-	case '}'	: 
-		out_types[work_state.IndexCurrentElement].Span.End	= work_state.IndexCurrentChar;
-		out_types.push_back({-1, ::gpk::JSON_FORMATTER_TYPE_LITERAL, {work_state.IndexCurrentChar, work_state.IndexCurrentChar}});
-	}
-	return 0; 
-}
-::gpk::error_t								jsonStringFormatParseLiteral	(::gpk::SJSONFormatterState & work_state, ::gpk::array_pod<::gpk::SJSONFormatterType> & out_types, const ::gpk::view_const_string& in_format)		{ 
-	in_format;
-	switch(work_state.CharCurrent) {
-	default		: break;
-	case '{'	: 
-		if(work_state.Escaping)
-			break;
-		out_types[out_types.size() - 1].Span.End	= work_state.IndexCurrentChar;
-		work_state.IndexCurrentElement				= out_types.push_back({-1, ::gpk::JSON_FORMATTER_TYPE_TOKEN, {work_state.IndexCurrentChar, work_state.IndexCurrentChar}});
-		// do work
-		break;
-	case '\\'	: 
-		if(false == work_state.Escaping) {
-			work_state.Escaping							= true;
-			return 0;
-		}
-		break;
-	}
-	work_state.Escaping							= false;
-	return 0; 
-}
-
-::gpk::error_t								jsonStringFormatParseStep		(::gpk::SJSONFormatterState & work_state, ::gpk::array_pod<::gpk::SJSONFormatterType> & out_types, const ::gpk::view_const_string& in_format)		{
-
-	if(work_state.InsideToken)
-		jsonStringFormatParseToken		(work_state, out_types, in_format);
-	else {
-		if(0 == out_types.size()) 
-			work_state.IndexCurrentElement = out_types.push_back({-1, ::gpk::JSON_FORMATTER_TYPE_LITERAL, {work_state.IndexCurrentChar, work_state.IndexCurrentChar}});
-
-		jsonStringFormatParseLiteral	(work_state, out_types, in_format);
-	}
-	return 0;
-}
-
-
-::gpk::error_t								jsonStringFormatParse			(::gpk::SJSONFormatterState & work_state, ::gpk::array_pod<::gpk::SJSONFormatterType> & out_types, const ::gpk::view_const_string& in_format)		{
-
-	for(work_state.IndexCurrentChar = 0; work_state.IndexCurrentChar < in_format.size(); ++work_state.IndexCurrentChar) {
-		work_state.CharCurrent						= in_format[work_state.IndexCurrentChar];
-		gpk_necall(jsonStringFormatParseStep(work_state, out_types, in_format), "Parse step failed.");
-	}
-	return 0;
-}
-
-::gpk::error_t								jsonStringFormat			(const ::gpk::view_const_string& format, const ::gpk::SJSONNode& input, ::gpk::array_pod<char_t>& )			{
-	format, input;
-	return 0;
-}
 
 int											main						()			{
 	static const ::gpk::view_const_string			testJson					= //"[{\"a\":[123 3,32,1156]}]";
@@ -123,7 +40,7 @@ int											main						()			{
 	case 'c': break;
 	case 'd': break;
 	}
-	const char			bleh[] = "";
+	const char										bleh[]						= "";
 	info_printf("%s", bleh);
 
 	::gpk::SJSONReader								jsonReader;
@@ -160,5 +77,9 @@ int											main						()			{
 	uint32_t										lenString						= view.size();
 	sprintf_s(bufferFormat, "Found test value {'%%s':%%%u.%us}. Node type: %%u (%%s). Node Span: {%%u, %%u}. Parent index: %%u. Object index: %%u.", lenString, lenString);
 	info_printf(bufferFormat, test_key.begin(), view.begin(), node.Type, ::gpk::get_value_label(node.Type).begin(), node.Span.Begin, node.Span.End, node.ParentIndex, indexOfElement, view.begin());
-	return 0;
+
+	::gpk::array_pod<char_t>						formatted;
+	::gpk::jsonStringFormat("asdkjasdkj{people.property[9].name}", *object, formatted);
+
+ 	return 0;
 }
