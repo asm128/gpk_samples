ff299059f8df4f9439c36166ff0af29ae03f28b6
asm128 <pabloarielz@outlook.com>
2019-06-24 03:05:14 -0300

    * Fixed missing code analysis configurations.

--- gpk_build_cl/main.cpp
+++ gpk_build_cl/main.cpp
@@ -1,9 +1,99 @@
+#define GPK_CONSOLE_LOG_ENABLED
+#define GPK_ERROR_PRINTF_ENABLED
+#define GPK_WARNING_PRINTF_ENABLED
+#define GPK_INFO_PRINTF_ENABLED
+
 #include "gpk_json.h"
 #include "gpk_storage.h"
+#include "gpk_find.h"
 
-int										main				(int argc, char** argv)			{
+struct SConfigBuild {
+	::gpk::array_obj<::gpk::view_const_string>			LibFile;
+};
+
+::gpk::error_t							findLastSlash					(const ::gpk::view_const_string & path) {
+	int32_t										indexOfStartOfFileName			= ::gpk::rfind('\\', path);
+	if(-1 != indexOfStartOfFileName) 
+		return indexOfStartOfFileName;
+												indexOfStartOfFileName			= ::gpk::rfind('//', path);
+	if(-1 != indexOfStartOfFileName)
+		return indexOfStartOfFileName;
+	return -1;
+}
+
+struct SBuildConfig {
+	::gpk::view_const_string				PathConfig						= {};
+	::gpk::view_const_string				FilenameConfig					= {};
+	::gpk::array_pod<byte_t>				JsonConfigOfBuild				= {};
+	::gpk::SJSONReader						TreeConfigOfBuild				= {};
+};
+
+::gpk::error_t							buildSources					(const SBuildConfig & app, const ::gpk::view_array<::gpk::array_pod<char_t>> & listOfSourceFileNames) {
+	(void)app;
+	char										bufferFormat [4096]				= {};
+	for(uint32_t iFile = 0; iFile < listOfSourceFileNames.size(); ++iFile) {
+		const ::gpk::array_pod<char_t>				& sourceFileName				= listOfSourceFileNames[iFile];
+		sprintf_s(bufferFormat, "Source to build: %%.%us.", sourceFileName.size());
+		info_printf(bufferFormat, sourceFileName.begin());
+	}
+	return 0;
+}
+
+::gpk::error_t							buildProjects					(const SBuildConfig & app, int32_t indexOfBuildObject, const ::gpk::view_const_string & projectCollection, const ::gpk::view_const_string & extension) {
+	::gpk::SPathContents						treeOfSolution;
+	int32_t										indexLibNode					= ::gpk::jsonObjectValueGet(*app.TreeConfigOfBuild[indexOfBuildObject], app.TreeConfigOfBuild.View, projectCollection);
+	if(-1 == indexLibNode) 
+		info_printf("No projects found for this configuration.");
+	else {
+		info_printf("- * -- Building %s sources with %s extension -- * -", projectCollection.begin(), extension.begin());
+		const ::gpk::SJSONNode						& collectionNode				= *app.TreeConfigOfBuild[indexLibNode];
+		for(uint32_t iChild = 0, countChild = collectionNode.Children.size(); iChild < countChild; ++iChild) {
+			::gpk::array_obj<::gpk::array_pod<char_t>>	listOfSourceFileNames;
+			::gpk::array_pod<char_t>					pathToLibSources				= app.PathConfig;
+			pathToLibSources.push_back('\\');
+			pathToLibSources.append(app.TreeConfigOfBuild.View[collectionNode.Children[iChild]->ObjectIndex]);
+			pathToLibSources.push_back(0);
+			info_printf("-- Sources path: '%s'.", pathToLibSources.begin());
+			::gpk::array_obj<::gpk::array_pod<char_t>>	listOfAllFileNames;
+			gpk_necall(::gpk::pathList({pathToLibSources.begin(), pathToLibSources.size() - 1}, listOfAllFileNames, false), "%s", "Unknown error.");
+			for(uint32_t iFile = 0; iFile < listOfAllFileNames.size(); ++iFile) {
+				::gpk::view_const_string					fileName						= {listOfAllFileNames[iFile].begin(), listOfAllFileNames[iFile].size()};
+				int32_t										offsetExtensionExpected			= ((int32_t)fileName.size() - (int32_t)extension.size());
+				int32_t										offsetExtensionFound			= ::gpk::find_sequence_pod(extension, fileName);
+				if(offsetExtensionExpected == offsetExtensionFound)
+					listOfSourceFileNames.push_back(fileName);
+			}
+			::buildSources(app, listOfSourceFileNames);
+		}
+	}
+	return 0;
+}
+
+::gpk::error_t							buildConfig						(const SBuildConfig & app) {
+	::gpk::view_const_string					extension						= ".cpp";
+	{
+		int32_t										indexOfBuildObject				= ::gpk::jsonArrayValueGet(*app.TreeConfigOfBuild[0], 0);
+		::buildProjects(app, indexOfBuildObject, "libs", extension);
+		::buildProjects(app, indexOfBuildObject, "dlls", extension);
+		::buildProjects(app, indexOfBuildObject, "exes", extension);
+	}
+	return 0;
+}
+
+int										main							(int argc, char** argv)			{
+	char										bufferFormat [4096]				= {};
 	ree_if(argc < 2, "USAGE: \n\t%s [path/to/solution]", argv[0]);
-	const ::gpk::view_const_string				solutionPath		= {};
-
+	{ // Build single configuration
+		SBuildConfig								configBuild						= {};
+		configBuild.FilenameConfig				= {argv[1], (uint32_t)-1};
+		ree_if(::gpk::fileToMemory(configBuild.FilenameConfig, configBuild.JsonConfigOfBuild), "Failed to open build file: '%s'.", configBuild.FilenameConfig.begin());
+		sprintf_s(bufferFormat, "Failed to read configuration file: %%.%us. Path: %%.%us. Contents:\n%%.%us", configBuild.FilenameConfig.size(), configBuild.PathConfig.size(), configBuild.JsonConfigOfBuild.size());
+		ree_if(::gpk::jsonParse(configBuild.TreeConfigOfBuild, {configBuild.JsonConfigOfBuild.begin(), configBuild.JsonConfigOfBuild.size()}), bufferFormat, configBuild.FilenameConfig.begin(), configBuild.PathConfig.begin(), configBuild.JsonConfigOfBuild.begin());
+		sprintf_s(bufferFormat, "Using configuration file: %%.%us. Path: %%.%us. Contents:\n%%.%us", configBuild.FilenameConfig.size(), configBuild.PathConfig.size(), configBuild.JsonConfigOfBuild.size());
+		info_printf(bufferFormat, configBuild.FilenameConfig.begin(), configBuild.PathConfig.begin(), configBuild.JsonConfigOfBuild.begin());
+		int32_t										indexOfLastSlash				= ::findLastSlash(configBuild.FilenameConfig);
+		configBuild.PathConfig					= (-1 == indexOfLastSlash) ? ".\\" : ::gpk::view_const_string{configBuild.FilenameConfig.begin(), (uint32_t)indexOfLastSlash};
+		::buildConfig(configBuild);
+	}
 	return 0;
 }
--- test_gpk_json/main.cpp
+++ test_gpk_json/main.cpp
@@ -19,17 +19,16 @@
 
 ::gpk::error_t								testJSONReader				()			{
 	static const ::gpk::view_const_string			testJson					= //"[{\"a\":[123 3,32,1156]}]";
-		//"\n[ { \"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
 		"\n            "
-		"\n[ {\"Bleh\": \"test\" }, {}, {   }, [  ]  , [], {\"a\":{}, \"b\":[123,]}"
-		"\n, {\"NameId\" : \"ASD\", \"Bleh\" :[234124,123,243234, 321   , 65198], \"Else\": [{\"Object\": false}, {}],\"Something\" : \"out\",}"
-		"\n, {\"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
-		"\n, {\"NameId\" : true, \"B\\\"leh\": null, \"Else\": false}		"
-		"\n, {\"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
-		"\n, {\"NameId\" : true, \"B\\\"leh\": null, \"Else\": false} "
-		"\n, {\"NameId\" : \"true\", \"Bleh\": \"null\", \"Else\": []} "
-		//"\n,{ \"NameId\" : 12344, \"Bleh\": \"\" \"\", \"Else\": \"false\", \"not\" : tres} "
-		"\n, { \"NameId\" : .123, \"Bleh\": -456 , \"Else\": -.759 } "
+		"\n[\t {\"Bleh\": \"test\" }, {}, {   }, [  ]  , [], {\"a\":{}, \"b\":[123,]}"
+		"\n,\t {\"NameId\" : \"ASD\", \"Bleh\" :[234124,123,243234, 321   , 65198], \"Else\": [{\"Object\": false}, {}],\"Something\" : \"out\",}"
+		"\n,\t {\"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
+		"\n,\t {\"NameId\" : true, \"B\\\"leh\": null, \"Else\": false}		"
+		"\n,\t {\"NameId\" : \"654\", \"Bleh\":21354, \"Else\\u1954\": \"in\"} "
+		"\n,\t {\"NameId\" : true, \"B\\\"leh\": null, \"Else\": false} "
+		"\n,\t {\"NameId\" : \"true\", \"Bleh\": \"null\", \"Else\": []} "
+		//"\n, { \"NameId\" : 12344, \"Bleh\": \"\" \"\", \"Else\": \"false\", \"not\" : tres} "
+		"\n,\t { \"NameId\" : .123, \"Bleh\": -456 , \"Else\": -.759 } "
 		"\n]"
 		"\n           "
 		;
@@ -91,14 +90,14 @@
 int											main						()	{
 	gpk_necall(::testJSONReader(), "%s", "Failed to read JSON!");
 
-	const ::gpk::view_const_string					inputJson					= 
+	const ::gpk::view_const_string					inputJsonEasy				= 
 		"["
-		"\n\t{\"child_selected\" : 2, \"color\" : \"red\", \"race\" : \"brown\", \"weight\" : 160"
+		"\n\t{\"child_selected\" : {\"index\" : 2, \"active\" : true}, \"color\" : \"red\", \"race\" : \"brown\", \"weight\" : 160"
 		"\n\t\t, \"parent\" : {\"name\" : \"lucas\"}"
 		"\n\t\t, \"children\": [\"marta\", \"venus\", \"crystal\"]"
 		"\n\t\t, \"height\" : \"1.56\", \"name\" : \"carlos\""
 		"\n\t},"
-		"\n\t{\"child_selected\" : 2, \"color\" : \"red\", \"race\" : \"brown\", \"weight\" : 160"
+		"\n\t{\"child_selected\" : {\"index\" : 2, \"active\" : true}, \"color\" : \"red\", \"race\" : \"brown\", \"weight\" : 160"
 		"\n\t\t, \"parent\" : {\"name\" : \"lucas\"}"
 		"\n\t\t, \"children\": [\"marta\", \"venus\", \"crystal\"]"
 		"\n\t\t, \"height\" : \"1.56\", \"name\" : \"carlos\""
@@ -107,39 +106,52 @@ int											main						()	{
 	{ // first-level expression resolution tests.
 		{
 			const ::gpk::view_const_string					format						= "I want to replace this (but not \\{this}): Guy name: {name}.";
-			gpk_necall(::testJSONFormatter(format, inputJson), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", format.begin(), inputJson.begin());
-			info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", format.begin(), inputJson.begin());
+			gpk_necall(::testJSONFormatter(format, inputJsonEasy), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", format.begin(), inputJsonEasy.begin());
+			info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", format.begin(), inputJsonEasy.begin());
 		}
 		{
 			const ::gpk::view_const_string					format						= "I want to replace this (but not \\{this}): Parent name: {parent.name}.";
-			gpk_necall(::testJSONFormatter(format, inputJson), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", format.begin(), inputJson.begin());
-			info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", format.begin(), inputJson.begin());
+			gpk_necall(::testJSONFormatter(format, inputJsonEasy), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", format.begin(), inputJsonEasy.begin());
+			info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", format.begin(), inputJsonEasy.begin());
 		}
 		char											bufferFormat	[1024]		= {};
 		for(uint32_t iChild = 0; iChild < 3; ++iChild) {
 			sprintf_s(bufferFormat, "I want to replace this (but not \\{this}): A child of {name}'s name: {children[%u]}.", iChild);
-			gpk_necall(::testJSONFormatter(bufferFormat, inputJson), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", bufferFormat, inputJson.begin());
-			info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", bufferFormat, inputJson.begin());
+			gpk_necall(::testJSONFormatter(bufferFormat, inputJsonEasy), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", bufferFormat, inputJsonEasy.begin());
+			info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", bufferFormat, inputJsonEasy.begin());
 		}
 	}
 
 	{ // multilevel expression resolution tests.
-		//const ::gpk::view_const_string					inputJson					= 
-		//	// ----- Simple test.
-		//	//"[{\"color\" : \"red\", \"race\" : \"brown\", \"weight\" : 160, \"children\" : {\"name\" : \"lucas\"}, \"height\" : \"1.56\", \"name\" : \"carlos\"}]";
-		//	//// ----- Heavy test.
-		//	"[	"
-		//	"\n	{ \"properties\" : [{ \"name\" : \"color\", \"type\" : \"string\"}, { \"name\" : \"age\", \"type\" : \"int\"} ]"
-		//	"\n	, \"selection\" : {\"index\" : 1 , \"active\" : true }"
-		//	"\n	, \"people\" : "
-		//	"\n		{ \"property\" : "
-		//	"\n			{ \"color\" : \"red\", \"age\" : 25 }"
-		//	"\n		}"
-		//	"\n	}"
-		//	"\n]";
-		//const ::gpk::view_const_string					format						= "I want to replace this (but not \\{this}): People age: {people.property.{properties[selection.index].name}} with the value of the property found in a JSON tree.";
-		//gpk_necall(::testJSONFormatter(format, inputJson), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", format.begin(), inputJson.begin());
-		//info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", format.begin(), inputJson.begin());
+		{
+			const ::gpk::view_const_string					format						= "I want to replace this (but not \\{this}): A child of carlos's name: {children[child_selected.index]}.";
+			gpk_necall(::testJSONFormatter(format, inputJsonEasy), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", format.begin(), inputJsonEasy.begin());
+			info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", format.begin(), inputJsonEasy.begin());
+		}
+		const ::gpk::view_const_string					inputJsonHard					= 
+			"[	"
+			"\n	{ \"properties\" : [{ \"name\" : \"color\", \"type\" : \"string\"}, { \"name\" : \"age\", \"type\" : \"int\"} ]"
+			"\n	, \"selection\" : {\"index\" : 1 , \"active\" : true }"
+			"\n	, \"people\" : "
+			"\n		[{ \"name\" : \"David\""
+			"\n		 , \"property\" : "
+			"\n			{ \"color\" : \"red\", \"age\" : 25 }"
+			"\n		},"
+			"\n		],"
+			"\n	},"
+			"\n	{ \"properties\" : [{ \"name\" : \"color\", \"type\" : \"string\"}, { \"name\" : \"age\", \"type\" : \"int\"} ]"
+			"\n	, \"selection\" : {\"index\" : 1 , \"active\" : false }"
+			"\n	, \"people\" : "
+			"\n		[{ \"name\" : \"David\""
+			"\n		 , \"property\" : "
+			"\n			{ \"color\" : \"red\", \"age\" : 25 }"
+			"\n		},"
+			"\n		],"
+			"\n	},"
+			"\n]";
+		const ::gpk::view_const_string					format						= "I want to replace this (but not \\{this}): {people[0].name}'s {properties[selection.index].name}: {people[0].property.{properties[selection.index].name}}.";
+		gpk_necall(::testJSONFormatter(format, inputJsonHard), "%s", "Failed to format string!""\nFormat: \n%s""\nInput JSON: \n%s", format.begin(), inputJsonHard.begin());
+		info_printf("Test succeeded:\nInput expression:\n%s\nInput JSON:\n%s", format.begin(), inputJsonHard.begin());
 	}
 	return 0;
 }
